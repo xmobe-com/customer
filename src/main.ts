35fc6aec42cc2e8c21761605a0c1b686acf7923c
Quang Anh <quanganh@aiti.com.vn>
2020-01-31 15:25:39 +0700

    make more change

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('start server');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('server listen on: http://localhost:3000');
 }
 bootstrap();
